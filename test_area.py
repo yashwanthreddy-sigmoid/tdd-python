9c4cbe5ef60b7bc7de0bf8a7c2034606aac7e10e
yashwanthreddy-sigmoid <98736166+yashwanthreddy-sigmoid@users.noreply.github.com>
2022-02-24 12:07:43 +0530

    Update test_area.py

--- test_area.py
+++ test_area.py
@@ -1,9 +1,9 @@
 import os.path
 import pytest
-from area import area
+from AreaFinder import AreaFinder
 from unittest.mock import MagicMock
 
-class Testclass:
+class AreaFinderTest:
     @pytest.fixture()
     def Area(self):
         Area = area()
@@ -32,4 +32,18 @@ class Testclass:
         with pytest.raises(Exception):
             Area.areaofsquare(0)
             Area.areaofsquare(-1)
+            
+     
+
+    def test_returns_correct_output(self, Area, monkeypatch):
+        mock_file = MagicMock()
+        mock_file.readline = MagicMock(return_value=100)
+        mock_open = MagicMock(return_value=mock_file)
+        monkeypatch.setattr("builtins.open", mock_open)
+        mock_exists = MagicMock(return_value =True)
+        monkeypatch.setattr("os.path.exists", mock_exists)
+        result = Area.fromfile("/Users/somalayashwanthreddy/Documents/python/yash.txt")
+        mock_open.assert_called_once_with("/Users/somalayashwanthreddy/Documents/python/yash.txt", "r")
+        assert result == 100
+
 
